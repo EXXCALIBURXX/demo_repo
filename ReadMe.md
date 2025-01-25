b83f3be66e1e4e7ee6257bebcb4c7324acaae185
unknown <anshitsingh377@gmail.com>
2025-01-25 19:01:43 +0530

    Added why.cpp

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,6 @@
 # Why am I alive?
 
 good question
+
+The real question is actually -
+## Why should I keep you alive ?
---
+++ why.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Why do we use git?" << endl;
+    return 0;
+}
