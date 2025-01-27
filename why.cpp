18cc6ceac41f5d133e560ed9a4f18d91c120db66
EXXCALIBURXX <anshitsingh377@gmail.com>
2025-01-27 18:27:48 +0530

    Updataed why.cpp

--- why.cpp
+++ why.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout << "Why do we use git?? ya tell me??" << endl;
+    cout << "Why do we use git?? ya tell me?? tell me !!!" << endl;
     return 0;
 }
